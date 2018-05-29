5a8f353c32c7a83e9ecbc29a298afb3cb25fd6d8
Chris Eddy <ChrisEddy@users.noreply.github.com>
2018-05-29 08:19:53 -0700

    Update libs, google-places.js refactor, icons added for tires

--- google-places.js
+++ google-places.js
@@ -2,17 +2,17 @@
 
     $.googlePlaces = function(element, options) {
 
-        var defaults = {
+        let defaults = {
             placeId: 'ChIJ6TAqRQUNj1QRmq8PdYMGVB0' // placeId provided by google api documentation
             , render: ['reviews']
-            , min_rating: 0
+            , min_rating: 5
             , max_rows: 0
             , rotateTime: false
         };
 
-        var plugin = this;
+        let plugin = this;
 
-        plugin.settings = {}
+        plugin.settings = {};
 
         var $element = $(element),
             element = element;
@@ -25,32 +25,32 @@
                 // render specified sections
                 if(plugin.settings.render.indexOf('reviews') > -1){
                     renderReviews(plugin.place_data.reviews);
-                    if(!!plugin.settings.rotateTime) {
+                    if(plugin.settings.rotateTime) {
                         initRotation();
                     }
                 }
             });
-        }
+        };
 
-        var initialize_place = function(c){
-            var map = new google.maps.Map(document.getElementById('map-plug'));
+        const initialize_place = function(c){
+            let map = new google.maps.Map(document.getElementById('map-plug'));
 
-            var request = {
+            let request = {
                 placeId: plugin.settings.placeId
             };
 
-            var service = new google.maps.places.PlacesService(map);
+            let service = new google.maps.places.PlacesService(map);
 
             service.getDetails(request, function(place, status) {
-                if (status == google.maps.places.PlacesServiceStatus.OK) {
+                if (status === google.maps.places.PlacesServiceStatus.OK) {
                     c(place);
                 }
             });
-        }
+        };
 
-        var sort_by_date = function(ray) {
+        const sort_by_date = function(ray) {
             ray.sort(function(a, b){
-                var keyA = new Date(a.time),
+                let keyA = new Date(a.time),
                     keyB = new Date(b.time);
                 // Compare the 2 dates
                 if(keyA < keyB) return -1;
@@ -58,35 +58,37 @@
                 return 0;
             });
             return ray;
-        }
+        };
 
-        var filter_minimum_rating = function(reviews){
-            for (var i = reviews.length -1; i >= 0; i--) {
+        const filter_minimum_rating = function(reviews){
+            for (let i = reviews.length -1; i >= 0; i--) {
                 if(reviews[i].rating < plugin.settings.min_rating){
                     reviews.splice(i,1);
                 }
             }
             return reviews;
-        }
+        };
 
-        var renderReviews = function(reviews){
+        const renderReviews = function(reviews){
             reviews = sort_by_date(reviews);
             reviews = filter_minimum_rating(reviews);
-            var html = "";
-            var row_count = (plugin.settings.max_rows > 0)? plugin.settings.max_rows - 1 : reviews.length - 1;
+            let html = "";
+            let row_count = (plugin.settings.max_rows > 0)? plugin.settings.max_rows - 1 : reviews.length - 1;
             // make sure the row_count is not greater than available records
             row_count = (row_count > reviews.length-1)? reviews.length -1 : row_count;
-            for (var i = row_count; i >= 0; i--) {
-                var stars = renderStars(reviews[i].rating);
-                var date = convertTime(reviews[i].time);
-                html = html+"<div class='review-item'><div class='review-meta'><span class='review-author'>"+reviews[i].author_name+"</span><span class='review-date'>"+date+"</span></div>"+stars+"<p class='review-text'>"+'"'+""+reviews[i].text+""+'"'+"</p></div>"
-            };
+            for (let i = row_count; i >= 0; i--) {
+                let stars = renderStars(reviews[i].rating);
+                let date = convertTime(reviews[i].time);
+                html = html+"<div class='review-item'><div class='review-meta'><span class='review-author'>" +
+                    ""+reviews[i].author_name+"</span><span class='review-date'>"+date+"</span></div>"+stars+"" +
+                    "<p class='review-text'>"+'"'+""+reviews[i].text+""+'"'+"</p></div>"
+            }
             $element.append(html);
-        }
+        };
 
-        var initRotation = function() {
-            var $reviewEls = $element.children('.review-item');
-            var currentIdx = $reviewEls.length > 0 ? 0 : false;
+        const initRotation = function() {
+            let $reviewEls = $element.children('.review-item');
+            let currentIdx = $reviewEls.length > 0 ? 0 : false;
             $reviewEls.hide();
             if(currentIdx !== false) {
                 $($reviewEls[currentIdx]).show();
@@ -98,42 +100,42 @@
                     $($reviewEls[currentIdx]).fadeIn('slow');
                 }, plugin.settings.rotateTime);
             }
-        }
+        };
 
-        var renderStars = function(rating){
-            var stars = "<div class='review-stars'><ul>";
+        const renderStars = function(rating){
+            let stars = "<div class='review-stars'><ul>";
 
             // fill in gold stars
-            for (var i = 0; i < rating; i++) {
+            for (let i = 0; i < rating; i++) {
                 stars = stars+"<li><i class='star'></i></li>";
-            };
+            }
 
             // fill in empty stars
             if(rating < 5){
-                for (var i = 0; i < (5 - rating); i++) {
+                for (let i = 0; i < (5 - rating); i++) {
                     stars = stars+"<li><i class='star inactive'></i></li>";
-                };
+                }
             }
             stars = stars+"</ul></div>";
             return stars;
-        }
+        };
 
-        var convertTime = function(UNIX_timestamp){
-            var a = new Date(UNIX_timestamp * 1000);
-            var months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
-            var time = months[a.getMonth()] + ' ' + a.getDate() + ', ' + a.getFullYear();
+        const convertTime = function(UNIX_timestamp){
+            let a = new Date(UNIX_timestamp * 1000);
+            let months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
+            let time = months[a.getMonth()] + ' ' + a.getDate() + ', ' + a.getFullYear();
             return time;
-        }
+        };
 
         plugin.init();
 
-    }
+    };
 
     $.fn.googlePlaces = function(options) {
 
         return this.each(function() {
-            if (undefined == $(this).data('googlePlaces')) {
-                var plugin = new $.googlePlaces(this, options);
+            if (undefined === $(this).data('googlePlaces')) {
+                let plugin = new $.googlePlaces(this, options);
                 $(this).data('googlePlaces', plugin);
             }
         });
